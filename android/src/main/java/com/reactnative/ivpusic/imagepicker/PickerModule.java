c0a44f0ba44412c66bb0e3c0aafac2c732360294
Simon Gough <scgough@users.noreply.github.com>
2021-10-05 15:30:47 +0100

    fix as per source master

--- android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerModule.java
+++ android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerModule.java
@@ -615,7 +615,7 @@ class PickerModule extends ReactContextBaseJavaModule implements ActivityEventLi
 
         // if compression options are provided image will be compressed. If none options is provided,
         // then original image will be returned
-        File compressedImage = compression.compressImage(options, path, original);
+        File compressedImage = compression.compressImage(this.reactContext, options, path, original);
         String compressedImagePath = compressedImage.getPath();
         BitmapFactory.Options options = validateImage(compressedImagePath);
         long modificationDate = new File(path).lastModified();
@@ -642,7 +642,7 @@ class PickerModule extends ReactContextBaseJavaModule implements ActivityEventLi
 
         return image;
     }
-    
+
     private void configureCropperColors(UCrop.Options options) {
         if (cropperActiveWidgetColor != null) {
             options.setActiveControlsWidgetColor(Color.parseColor(cropperActiveWidgetColor));
